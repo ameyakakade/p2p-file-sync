043a708b4de210e779145f3c2d671bcfd6d1c516
ameyakakade <ameyakakade@gmail.com>
2026-07-28 22:23:02 +0530

    Add openssl source code

---
+++ thirdparty/openssl-4.0.1/test/buildtest_c_posix_time-bin-buildtest_posix_time.d
@@ -0,0 +1,4 @@
+test/buildtest_c_posix_time-bin-buildtest_posix_time.o: \
+  test/buildtest_posix_time.c include/openssl/opensslconf.h \
+  include/openssl/configuration.h include/openssl/macros.h \
+  include/openssl/opensslv.h include/openssl/posix_time.h
