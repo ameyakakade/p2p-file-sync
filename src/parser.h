5a3730bd323c8d5d0559fa5f863ea29f64dd25fe
Ameya <ameyakakade@gmail.com>
2026-08-18 01:14:11 +0530

    Sending merkle tree over network 'works'

---
+++ src/parser.h
@@ -0,0 +1,101 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstdint>
+#include <cassert>
+
+struct Sp {
+    const char* string;
+    int loc;
+};
+
+char gc(Sp* sp) {
+    return sp->string[sp->loc];
+}
+
+Sp createSp(const char* s) {
+    return {.string = s, .loc = 0};
+}
+
+void printSp(Sp* sp) {
+    const char* sn = sp->string + sp->loc;
+    printf(sn);
+}
+
+void eatSpaces(Sp* sp) {
+    while(gc(sp) == ' ' && gc(sp) != '\0') {
+        sp->loc = sp->loc + 1;
+    }
+}
+
+void charParser(Sp* sp, char c) {
+    eatSpaces(sp);
+    if(gc(sp) == c && gc(sp) != '\0') {
+        sp->loc = sp->loc + 1;
+    }
+}
+
+void keywordParser(Sp* sp, const char* keyword) {
+    eatSpaces(sp);
+    Sp kp = createSp(keyword);
+    while(gc(sp) == gc(&kp) && gc(sp) != '\0' && gc(&kp) != '\0') {
+        sp->loc = sp->loc + 1;
+        kp.loc = kp.loc + 1;
+    }
+}
+
+uint64_t natParser(Sp* sp) {
+    eatSpaces(sp);
+    uint64_t a = 0;
+    while('0' <= gc(sp) && gc(sp) <= '9' && gc(sp) != '\0') {
+        a *= 10;
+        a += gc(sp) - '0';
+        sp->loc = sp->loc + 1;
+    }
+    return a;
+}
+
+std::string stringParser(Sp* sp) {
+    eatSpaces(sp);
+    charParser(sp, '"');
+    std::string a;
+    while(gc(sp) != '"') {
+        a += gc(sp);
+        sp->loc = sp->loc + 1;
+    }
+    charParser(sp, '"');
+    return a;
+}
+
+bool boolParser(Sp* sp) {
+    eatSpaces(sp);
+    std::string a;
+    while(gc(sp) != ',') {
+        a += gc(sp);
+        sp->loc = sp->loc + 1;
+    }
+    if(a == "false") return false;
+    if(a == "true") return true;
+    assert(false);
+}
+
+#define FIELD_PARSE(sp, keyword, parser)        \
+    keywordParser(sp, keyword);                 \
+    charParser(sp, ':');                        \
+    parser                                      \
+    charParser(sp, ',');
+
+#define ARRAY_PARSE(sp, parser)                 \
+    charParser(sp, '[');                        \
+    while(gc(sp) != ']') {                      \
+        parser                                  \
+            charParser(sp, ',');                \
+    }                                           \
+    charParser(sp, ']');
+
+#define OBJECT_PARSE(sp, parser)                \
+    eatSpaces(sp);                              \
+    charParser(sp, '{');                        \
+    parser                                      \
+    charParser(sp, '}');
